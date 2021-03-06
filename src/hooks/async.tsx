c85161eed49b007daea9b94aecbce6250bb11bac
AnxinYang <anxinyang114@gmail.com>
2021-03-05 16:41:37 -0800

    Update async.tsx

--- src/hooks/async.tsx
+++ src/hooks/async.tsx
@@ -1,19 +1,18 @@
 import { useEffect, useState } from "react";
 
 export function useAsync(fn: Function): [boolean, () => void] {
-    const [stage, setStage] = useState(0);
+    const [isDone, setIsDone] = useState(false);
     const [ts, setTs] = useState(Date.now());
     const run = async () => {
-        setStage(1)
         await fn();
-        setStage(2);
+        setIsDone(true)
     }
     useEffect(() => {
         run();
     }, [ts]);
 
-    return [stage === 2, () => {
-        setStage(0);
+    return [isDone, () => {
+        setIsDone(false);
         setTs(Date.now())
     }]
 }
