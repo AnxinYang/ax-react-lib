b4ba27633b2b4a2270243754170de9d5cd491e60
AnxinYang <anxinyang114@gmail.com>
2021-03-05 16:40:17 -0800

    limit useEffect

--- src/hooks/async.tsx
+++ src/hooks/async.tsx
@@ -2,15 +2,18 @@ import { useEffect, useState } from "react";
 
 export function useAsync(fn: Function): [boolean, () => void] {
     const [stage, setStage] = useState(0);
+    const [ts, setTs] = useState(Date.now());
     const run = async () => {
         setStage(1)
         await fn();
         setStage(2);
     }
     useEffect(() => {
-        if (stage !== 0) return;
         run();
-    }, [stage]);
+    }, [ts]);
 
-    return [stage === 2, () => setStage(0)]
+    return [stage === 2, () => {
+        setStage(0);
+        setTs(Date.now())
+    }]
 }
